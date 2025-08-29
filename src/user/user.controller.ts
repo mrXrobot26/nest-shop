b0e498a1c95d3a97a0b902be2a1ac33e78befa80
Abdelaziz Elbessa <zezoelbessa10@gmail.com>
2025-08-29 11:58:44 +0300

    simple crud users complete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Serialize } from 'src/common/Decorators/Serialize.decorator';
 import { getUserDto } from './dto/get-user.dto copy';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('/')
+  @Post()
   @Serialize(getUserDto)
   async createUser(@Body() createUserDto: CreateUserDto) {
     const user = await this.userService.createUser(createUserDto);
     return user;
+  }
+
+  @Get()
+  @Serialize(getUserDto)
+  async getAllUser() {
+    const users = await this.userService.getAllUser();
+    return users;
   }
 
   @Get('/:id')
@@ -20,5 +28,17 @@ export class UserController {
   async getUser(@Param('id', ParseIntPipe) id: number) {
     const user = await this.userService.getUserById(id);
     return user;
+  }
+
+  @Patch('/:id')
+  @Serialize(getUserDto)
+  async updateUser(@Param('id', ParseIntPipe) id: number, @Body() updatedUser: UpdateUserDto) {
+    const user = await this.userService.updateUser(id, updatedUser);
+    return user;
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -17,8 +18,33 @@ export class UserService {
     return createdUser;
   }
 
+  async getAllUser() {
+    const users = await this.userRepo.find();
+    return users;
+  }
+
   async getUserById(id: number) {
     const user = await this.userRepo.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
     return user;
+  }
+
+  async updateUser(id: number, updatedUser: UpdateUserDto) {
+    const user = await this.userRepo.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    Object.assign(user, updatedUser);
+    return await this.userRepo.save(user);
+  }
+
+  async deleteUser(id: number) {
+    const result = await this.userRepo.delete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return { message: `User with ID ${id} deleted successfully` };
   }
 }
